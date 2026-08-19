68528fbb0f844ec4253336eae9b47726364422de
youswiftlmao <ykhanbugatti@gmail.com>
2026-08-19 16:08:08 +0400

    re writing command to do more then hardcoded stuff

--- commands.py
+++ commands.py
@@ -1,37 +1,13 @@
 import subprocess
 import webbrowser
-
+import os
 
 def open_application(name: str) -> str:
-    """
-    Opens an application on the user's computer.
-
-    Args:
-        name: The name of the application to open.
-    """
-
-    applications = {
-        "calculator": "calc.exe",
-        "notepad": "notepad.exe",
-        "paint": "mspaint.exe",
-    }
 
     name = name.lower().strip()
 
-    if name in applications:
-        subprocess.Popen(applications[name])
-        return f"Opened {name}."
-
-    return f"I don't know how to open {name}."
-
 
 def open_website(url: str) -> str:
-    """
-    Opens a website in the user's default browser.
-
-    Args:
-        url: The complete website URL.
-    """
 
     webbrowser.open(url)
 
